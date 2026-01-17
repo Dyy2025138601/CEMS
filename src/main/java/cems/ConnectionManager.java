fa743451ecbe3c879aec218b14552a8b90ee30dd
Dyy2025138601 <2025138601@student.uitm.edu.my>
2026-01-17 07:36:37 +0000

    initial commit

--- src/main/java/cems/ConnectionManager.java
+++ src/main/java/cems/ConnectionManager.java
@@ -2,38 +2,37 @@ package cems;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class ConnectionManager {
 
     private static final String DB_DRIVER = "org.postgresql.Driver";
-
     private static final String DB_URL =
-        "jdbc:postgresql://c3v5n5ajfopshl.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dubdf986cmc3n";
-
-    private static final String DB_USER = "udq3kr6kb5d95m";
-    private static final String DB_PASSWORD =
-        "p7d05481b5598901f9dc6b0cfeeed76e924796d37cf086fe9a23f6d81e36ee68";
+            "jdbc:postgresql://localhost:5432/cems";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "postgres";
 
     public static Connection getConnection() {
-        Connection con = null;
+
+        Connection conn = null;
 
         try {
             Class.forName(DB_DRIVER);
+            System.out.println("✅ PostgreSQL driver loaded");
 
-            con = DriverManager.getConnection(
+            conn = DriverManager.getConnection(
                     DB_URL,
                     DB_USER,
                     DB_PASSWORD
             );
 
-            System.out.println("PostgreSQL connected successfully ✅");
+            conn.setAutoCommit(false);
+            System.out.println("✅ Database connected");
 
         } catch (Exception e) {
-            System.out.println("PostgreSQL connection FAILED ❌");
+            System.out.println("❌ DATABASE CONNECTION FAILED");
             e.printStackTrace();
         }
 
-        return con;
+        return conn;
     }
 }
